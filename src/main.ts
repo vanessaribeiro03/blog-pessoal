3650e32b25a4e44c17a59d5835c84a19e55901d5
Vanessa Ribeiro <ribeirovs2000@outlook.com>
2025-01-14 14:16:06 -0300

    feat: finalizando o crud

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  process.env.TZ = '-03:00'
+
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();
--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,3 +1,4 @@
+import { Transform, TransformFnParams } from "class-transformer";
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
@@ -6,10 +7,12 @@ export class Postagem{
     @PrimaryGeneratedColumn()  // AUTO_INCREMENT PRIMARY KEY
     id: number;
 
+    @Transform(({ value }: TransformFnParams) => value?.trim())
     @IsNotEmpty()  // validação dos dados do objeto
     @Column({length: 100, nullable: false})  // VARCHAR(100) NOT NULL
     titulo: string;
 
+    @Transform(({ value }: TransformFnParams) => value?.trim())
     @IsNotEmpty()  // validação dos dados do objeto
     @Column({length: 1000, nullable: false})  // VARCHAR(100) NOT NULL
     texto: string;
